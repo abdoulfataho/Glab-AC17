6ba05e43c01c2c5e474bcf965f54d9b328c729ba
abdoulfataho <yamyaogofataho@gmail.com>
2024-09-02 12:29:47 -0400

    final

---
+++ UploadPDFFile/src/main/java/com/example/UploadPDFFile/FileUploadModel.java
@@ -0,0 +1,11 @@
+package com.example.UploadPDFFile;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileUploadModel {
+    private String uploadedFile;
+}
